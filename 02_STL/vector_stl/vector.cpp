ad868340789df1625e294b4e05ef7e6ceb8858ed
shyamsnp1804 <snp18032004@gmail.com>
2026-01-07 14:48:45 +0530

    learned vector stl

--- 02_STL/vector_stl/vector.cpp
+++ 02_STL/vector_stl/vector.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void explainVector()
 {
-  vector<int> vec;
+  vector<int> vec = {4, 5, 6, 7, 8};
   vec.push_back(3); // push_back() inserts element in vector
   vec.push_back(2);
   vec.push_back(1);
@@ -42,6 +42,56 @@ void explainVector()
   // print vector elements
   cout << "vector elements are -> ";
   for (auto i : vec)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  // reverse iterator
+  auto reverseBegin = vec.rbegin(); // rbegin() give location of last element
+  auto reverseEnd = vec.rend();     // rend() give location just before first element
+  cout << "elements in reverse order - > ";
+  for (auto i = reverseBegin; i < reverseEnd; i++)
+  {
+    cout << *i << " ";
+  }
+  cout << endl;
+
+  // printing element from and to specific index
+  vector<int> dupVec(vec.begin() + 1, vec.end() - 1); // remember-> vec.begin() include first element and vec.end() exclude the last element because c++ iterators ranges are : [first , last) means include first and exclude last element
+  for (auto i : dupVec)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  // front
+  cout << "front element is -> " << vec.front() << endl;
+
+  // back
+  cout << "last element is -> " << vec.back() << endl;
+
+  // pop_back
+  vec.pop_back(); // pop last element
+  cout << "last element after pop_back is -> " << vec.back() << endl;
+
+  // erase -> we can use it to remove specific element from vector or also delete in range
+  vec.erase(vec.begin() + 1, vec.end() - 1);
+  for (auto i : vec)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+  // clear-> The clear function removes all elements from the vector, making it empty.
+  vec.clear();
+
+  cout << "size of vector -> " << vec.size() << endl;
+
+  // insert
+  vector<int> vec2 = {11, 22, 33, 44};
+  vec2.insert(vec2.begin(), 10);
+  cout << "element of vec2 after insertion -> ";
+  for (auto i : vec2)
   {
     cout << i << " ";
   }
