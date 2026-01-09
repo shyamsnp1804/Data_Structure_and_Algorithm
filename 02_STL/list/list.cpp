feaa09ed2a8fafd9861cdc91cb8342a28cac52d3
shyamsnp1804 <snp18032004@gmail.com>
2026-01-09 21:32:08 +0530

    learned more about list stl

--- 02_STL/list/list.cpp
+++ 02_STL/list/list.cpp
@@ -6,6 +6,14 @@ void explainList()
   list<int> lst = {10, 20, 30};
   lst.push_front(11);    // push_front() insert at the beginning
   lst.emplace_front(12); // emplace_front() construct at the beginning
+  cout << "elements of list are -> ";
+  for (auto i : lst)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+  lst.pop_front();
+  cout << "elements after poping first element -> ";
   for (auto i : lst)
   {
     cout << i << " ";
