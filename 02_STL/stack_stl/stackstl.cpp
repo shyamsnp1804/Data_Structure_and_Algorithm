d630adf9c7fc11db69ec1f663aa6643fdac02e58
shyamsnp1804 <snp18032004@gmail.com>
2026-01-12 22:07:13 +0530

    learned stack stl

--- 02_STL/stack_stl/stackstl.cpp
+++ 02_STL/stack_stl/stackstl.cpp
@@ -10,7 +10,19 @@ void explainStack()
   st.pop();                        // deletes 7 from top of the stack
   cout << st.top() << " " << endl; // print 4
   st.pop();                        // deletes 4 from stack
-  cout << st.top() << " ";         // print nothing -> empty stack
+  // cout << st.top() << " " << endl; // print nothing -> empty stack => causes undefined behavior and next line of code will not be executed
+
+  st.push(1);
+  st.push(3);
+  st.push(5);
+  st.push(7);
+  st.push(11);
+  // printing top element and poping element
+  while (st.empty() == false)
+  {
+    cout << st.top() << " ";
+    st.pop();
+  }
 }
 
 int main()
