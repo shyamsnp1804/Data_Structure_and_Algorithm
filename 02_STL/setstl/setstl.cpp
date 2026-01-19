44f6cb29c0dac18d354dfbd6d3b6ac79579fab7c
shyamsnp1804 <snp18032004@gmail.com>
2026-01-19 22:31:24 +0530

    learned set stl

--- 02_STL/setstl/setstl.cpp
+++ 02_STL/setstl/setstl.cpp
@@ -8,20 +8,52 @@ void explainSet()
   st.insert(14);
   st.insert(12);
   st.insert(13);
-
+  cout << "elements of set are -> ";
   for (auto i : st)
   {
     cout << i << " ";
   }
   cout << endl;
+
+  // finding element in set
   auto i = st.find(14);
-  if (i != st.end())
+  if (i != st.end()) // s.end() → points one position past last element in set
   {
     cout << "found " << *i << " ";
   }
   else
   {
     cout << "not found";
+  }
+  cout << endl;
+
+  // erase
+  st.erase(14);
+  for (auto i : st)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+  // lower_bound(x) -> returns First element ≥ x
+  auto it = st.lower_bound(14);
+  if (it != st.end())
+  {
+    cout << "lower_bound element is -> " << *it << "";
+  }
+  else
+  {
+    cout << "element not found";
+  }
+  cout << endl;
+  // upper_bound(x) -> returns First element > x
+  auto itub = st.upper_bound(12);
+  if (itub != st.end())
+  {
+    cout << "upper_bound element is -> " << *itub << "";
+  }
+  else
+  {
+    cout << "element not found";
   }
 }
 
