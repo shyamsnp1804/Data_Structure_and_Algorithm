6d64c461de69fa9095d53d66acd755fa2df99434
shyamsnp1804 <snp18032004@gmail.com>
2026-01-25 22:31:46 +0530

    learned something more about stack stl (LIFO)

--- 02_STL/stack_stl/stackstl.cpp
+++ 02_STL/stack_stl/stackstl.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void explainStack()
 {
-  stack<int> st;
+  stack<int> st, st2, st3;
   st.push(4);
   st.push(7);
   cout << st.top() << " " << endl; // print 7
@@ -18,11 +18,39 @@ void explainStack()
   st.push(7);
   st.push(11);
   // printing top element and poping element
+  cout << "elements of st are -> ";
   while (st.empty() == false)
   {
     cout << st.top() << " ";
     st.pop();
   }
+  cout << endl;
+  st2.push(12);
+  st2.push(14);
+  st2.push(16);
+  st2.push(18);
+  st2.push(20);
+
+  st3.push(13);
+  st3.push(15);
+  st3.push(17);
+  st3.push(19);
+  st3.push(30);
+
+  // swap
+  st3.swap(st2);
+  cout << "Top element of st2 after swap: " << st2.top() << endl;
+  cout << "Top element of st3 after swap: " << st3.top() << endl;
+
+  // size
+  cout << "Size of st: " << st.size() << endl;
+  cout << "Size of st2: " << st2.size() << endl;
+  cout << "Size of st3: " << st3.size() << endl;
+
+  // empty
+  cout << "Is st empty? " << (st.empty() ? "yes" : "no") << endl;
+  cout << "Is st2 empty? " << (st2.empty() ? "yes" : "no") << endl;
+  cout << "Is st3 empty? " << (st3.empty() ? "yes" : "no") << endl;
 }
 
 int main()
