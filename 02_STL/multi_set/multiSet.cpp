5ac9a5643b759e9e2ae783b9817a1fc3a95bee11
shyamsnp1804 <snp18032004@gmail.com>
2026-01-27 21:07:12 +0530

    knowledge ++ of multiset stl

--- 02_STL/multi_set/multiSet.cpp
+++ 02_STL/multi_set/multiSet.cpp
@@ -9,6 +9,10 @@ void explainMultiSet()
   ms.insert(89);
   ms.insert(96);
   ms.insert(88);
+  ms.insert(88);
+  ms.insert(99);
+  ms.insert(99);
+  ms.insert(99);
   ms.insert(99);
   cout << "elements of multiset -> ";
   for (auto i : ms)
@@ -18,7 +22,7 @@ void explainMultiSet()
   cout << endl;
 
   // erase
-  ms.erase(99);
+  ms.erase(99); //  erase all occurence of an element here it is 99
   cout << "elements of multiset after erasing -> ";
   for (auto i : ms)
   {
@@ -26,14 +30,23 @@ void explainMultiSet()
   }
   cout << endl;
 
-  // erasing specific element
+  // erase only one occurence of an element
+  ms.erase(ms.find(88)); // erase only first 88 because ms.find() is an iterator it will point to location
+  cout << "elements of ms after erasing only one 88 -> ";
+  for (auto i : ms)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  // erasing elements in range
   auto it1 = ms.begin();
   it1++;
   auto it2 = ms.end();
   it2--;
 
   ms.erase(it1, it2);
-  cout << "elements of multiset after erasing specific elements -> ";
+  cout << "elements of multiset after erasing in range -> ";
   for (auto iter : ms)
   {
     cout << iter << " ";
