df8858ba55ac265912ee784aa03d2a36e11c0c8c
shyamsnp1804 <snp18032004@gmail.com>
2026-04-01 19:57:26 +0530

    hash

--- 11_Hashing/02_Longest_subarray_with_sum_K/optimal(pos+neg).cpp
+++ 11_Hashing/02_Longest_subarray_with_sum_K/optimal(pos+neg).cpp
@@ -1,34 +1,72 @@
+/* Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
+
+Example
+
+Input: nums = [10, 5, 2, 7, 1, 9],  k=15
+
+Output: 4
+
+Explanation:
+
+The longest sub-array with a sum equal to 15 is [5, 2, 7, 1], which has a length of 4. This sub-array starts at index 1 and ends at index 4, and the sum of its elements (5 + 2 + 7 + 1) equals 15. Therefore, the length of this sub-array is 4. */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestSubarray(vector<int> &nums, int k) {
-        
-        unordered_map<long long, int> preSumMap; // prefix sum → first index
-        long long sum = 0;
-        int maxLen = 0;
+  int longestSubarray(vector<int> &nums, int k)
+  {
+    int n = nums.size();
 
-        // 🔥 Important trick: handles subarrays starting from index 0
-        preSumMap[0] = -1;
+    map<int, int> preSumMap; // stores prefix sum as key and its first occurrence index as value
 
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];  // build prefix sum
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++)
+    {
+      // calculate the prefix sum till index i
+      sum += nums[i];
 
-            long long rem = sum - k;
+      // if the sum equals k, update maxLen
+      if (sum == k)
+      {
+        maxLen = max(maxLen, i + 1);
+      }
 
-            // check if required prefix exists
-            if (preSumMap.find(rem) != preSumMap.end()) {
-                int len = i - preSumMap[rem];
-                maxLen = max(maxLen, len);
-            }
+      // calculate the sum of remaining part i.e., sum - k
+      int rem = sum - k;
 
-            // store only first occurrence of prefix sum
-            if (preSumMap.find(sum) == preSumMap.end()) {
-                preSumMap[sum] = i;
-            }
-        }
+      // calculate the length and update maxLen
+      if (preSumMap.find(rem) != preSumMap.end())
+      {
+        int len = i - preSumMap[rem]; // preSumMap[rem] is index where prefix sum=(sum - k) was previously seen
+        maxLen = max(maxLen, len);
+      }
 
-        return maxLen;
+      // update the map if sum is not already present
+      if (preSumMap.find(sum) == preSumMap.end())
+      {
+        preSumMap[sum] = i;
+      }
     }
+
+    return maxLen;
+  }
 };
+
+int main()
+{
+  vector<int> a = {-1, 1, 1};
+  int k = 1;
+
+  Solution solution;
+
+  int len = solution.longestSubarray(a, k);
+
+  cout << "The length of the longest subarray is: " << len << "\n";
+  return 0;
+}
+
+// T.C-> O(n log n) and S.C-> O(n)
