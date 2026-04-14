3c63f94f90cf5bfac5e582654f43667b77b82f72
shyamsnp1804 <snp18032004@gmail.com>
2026-04-14 20:26:12 +0530

    binary search

--- 12_Binary_search/03_On_answers/03_Find_the_smallest_divisor/binary.cpp
+++ 12_Binary_search/03_On_answers/03_Find_the_smallest_divisor/binary.cpp
@@ -3,9 +3,43 @@ using namespace std;
 
 class Solution
 {
+private:
+  int findSum(vector<int> &nums, int divisor)
+  {
+    int sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      sum += ceil((double)(nums[i]) / (double)(divisor));
+    }
+    return sum;
+  }
+
 public:
   int smallestDivisor(vector<int> &nums, int limit)
   {
+    int low = 1;
+    int high = *max_element(nums.begin(), nums.end());
+    int ans = high;
+
+    while (low <= high)
+    {
+
+      int mid = low + (high - low) / 2;
+
+      int sum = findSum(nums, mid);
+
+      if (sum <= limit)
+      {
+        ans = mid;
+        high = mid - 1;
+      }
+      else
+      {
+        low = mid + 1;
+      }
+    }
+    return ans;
   }
 };
 
